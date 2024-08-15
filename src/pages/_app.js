a130d6fda2b2d406bb8ac25e29994bc45c2897ca
lasha <lasha.dodashvili@gmai.com>
2024-08-15 14:59:28 +0400

    update environment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,9 +8,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 
 // Load your publishable key from Stripe
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
-);
+const stripePromise = loadStripe(process.env.NEXT_STRIPE_PUBLISHABLE_KEY);
 
 export default function App({ Component, pageProps }) {
   return (
